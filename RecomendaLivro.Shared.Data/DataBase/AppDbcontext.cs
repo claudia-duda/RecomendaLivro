38b06b90c620dda833549a2efa116a4873d1647e
--global <claudiaeduarda_duda@hotmail.com>
2024-08-22 16:37:33 -0300

    Finishing setup execution using local dbcontext

--- RecomendaLivro.Shared.Data/DataBase/AppDbcontext.cs
+++ RecomendaLivro.Shared.Data/DataBase/AppDbcontext.cs
@@ -6,16 +6,32 @@ namespace RecomendaLivro.Shared.Data.DataBase
 {
     public class AppDbContext : IdentityDbContext<UserAuthorized, Profile, int>
     {
+        public DbSet<AccountModel> Account { get; set; }
+        private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSoundV0;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
         public AppDbContext()
         {
         }
-        public DbSet<AccountModel> Account { get; set; }
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+           
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured)
+            {
+                return;
+            }
+            optionsBuilder
+                .UseSqlServer(connectionString)
+                .UseLazyLoadingProxies();
+        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-
-            // Configurações adicionais, como chaves, relações, etc.
+            modelBuilder.Entity<AccountModel>();
         }
     }
 
